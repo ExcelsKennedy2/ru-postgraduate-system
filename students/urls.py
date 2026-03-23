5db4a628649621d5a7e7271354424f39551be526
ExcelsKennedy <excelskennedy@gmail.com>
2026-03-23 11:46:20 +0300

    removed pdf functions

--- students/urls.py
+++ students/urls.py
@@ -4,7 +4,7 @@ from .views import student_dashboard, submissions_api, download_submission, down
 urlpatterns = [
     path('dashboard/', student_dashboard, name='student_dashboard'),
     path('api/submissions/', submissions_api, name='submissions_api'),
-    path('submissions/<int:submission_id>/download/', download_submission, name='download_submission'),
-    path('submissions/download-all/', download_all_submissions, name='download_all_submissions'),
+    # path('submissions/<int:submission_id>/download/', download_submission, name='download_submission'),
+    # path('submissions/download-all/', download_all_submissions, name='download_all_submissions'),
     path('book-presentation/', book_presentation, name='book_presentation'),
 ]
